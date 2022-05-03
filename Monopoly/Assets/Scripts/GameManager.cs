4d84eee580e2358e0376da95224f95eda3d89dbf
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2022-05-03 16:40:20 +0700

    Update GameManager.cs

--- Monopoly/Assets/Scripts/GameManager.cs
+++ Monopoly/Assets/Scripts/GameManager.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    public Player player;
+    public Route route;
+    public enum NodeType
     {
-        
+        Place,
+        Chest,
+        Pay,
+        Jail,
+        Factory,
+        Station
+    };
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && !player.IsMoving())
+        {
+            int steps = RollDices(2);
+            StartCoroutine(player.Move(steps));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private int RollDices(int diceNum)
     {
-        
+        int sum = 0;
+        for (int i = 0; i < diceNum; i++)
+        {
+            int diceValue = Random.Range(1, 7);
+            Debug.Log("Dice " + i.ToString() + ": " + diceValue.ToString());
+            sum += diceValue;
+        }
+        return sum;
     }
+
 }
