749b1bda001af0e206a2f95e91815bfc2e89dca7
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2022-05-30 19:36:39 +0700

    add new dialog class

---
+++ Monopoly/Assets/Scripts/QuestionDialog.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestionDialog : MonoBehaviour
+{
+    public static QuestionDialog Instance { get; private set; }
+
+    private TextMeshProUGUI text;
+    private Button yesButton;
+    private Button noButton;
+
+    private void Awake()
+    {
+        Instance = this;
+        text = transform.Find("Text").GetComponent<TextMeshProUGUI>();
+        yesButton = transform.Find("Yes Button").GetComponent<Button>();
+        noButton = transform.Find("No Button").GetComponent<Button>();
+
+        ShowQuestion("Do you want to buy this?", () => { Debug.Log("Yes"); }, () => { Debug.Log("No"); });
+    }
+
+    public void ShowQuestion(string _text, Action yesAction, Action noAction)
+    {
+        text.text = _text;
+        yesButton.onClick.AddListener(() =>
+        {
+            Hide();
+            yesAction();
+        });
+        noButton.onClick.AddListener(() =>
+        {
+            Hide();
+            noAction();
+        });
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
